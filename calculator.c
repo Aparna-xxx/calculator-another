ae45e9225257a9b8da4dc95ebbd5d09072b3df8b
Aparna <aparnaramanathan14@gmail.com>
2023-08-12 11:09:55 +0530

    first commit

---
+++ calculator.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+int add(int n1,int n2);
+int sub(int n1, int n2);
+
+int main(){
+    int a,b;
+    printf("Enter the value for first operand:");
+    scanf("%d",&a);
+    printf("Enter the value for second operand:");
+    scanf("%d",&b);
+
+    int sum=add(a,b);
+    printf("\nThe sum of two numbers is: %d",sum);
+    int difference=sub(a,b);
+    printf("\nThe difference between the two numbers is: %d",difference);
+    //mul(a,b);
+    //div(a,b);
+}
+
+int add(int n1, int n2){
+    return(n1+n2);
+}
+
+int sub(int n1, int n2){
+    return (n1-n2);
+}
+
